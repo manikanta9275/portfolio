ba5c4940e1675562bcc3e7294b9f443eef4a59f4
kotti manikanta <manikantakotti9275@gmail.com>
2024-08-17 16:26:42 +0530

    Your commit message here

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,14 +6,16 @@ import { ProjectCard } from "./ProjectCard";
 export const Projects = () => {
   return (
     <section className={styles.container} id="projects">
-      <div className={styles.line}></div>
-      <h2 className={styles.title}>Projects</h2>
-      <div className={styles.des}>Explore my diverse projects, where you can see my skills, creativity, and hard work. Each project reflects my focus on delivering high-quality solutions and solving problems effectively.</div>
-      <div className="gap">
-        <div className={styles.projects}>
-          {projects.map((project, id) => (
-            <ProjectCard key={id} project={project} />
-          ))}
+      <div className="incon">
+        <div className={styles.line}></div>
+        <h2 className={styles.title}>Projects</h2>
+        <div className={styles.des}>Explore my diverse projects, where you can see my skills, creativity, and hard work. Each project reflects my focus on delivering high-quality solutions and solving problems effectively.</div>
+        <div className="gap">
+          <div className={styles.projects}>
+            {projects.map((project, id) => (
+              <ProjectCard key={id} project={project} />
+            ))}
+          </div>
         </div>
       </div>
     </section>
